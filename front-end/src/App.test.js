bd5cf523d6a89b6b514e2a6dd103f9043ea8d2ce
Scott van Leeuwen <78891914+svanlee@users.noreply.github.com>
2024-04-29 15:58:55 -0400

    improve

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,14 +1,21 @@
 import { render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 import "@testing-library/jest-dom/extend-expect";
 
+const renderWithHistory = (Component, route = "/") => {
+  const history = createMemoryHistory();
+  history.push(route);
+
+  return {
+    ...render(<Router location={history.location} navigator={history}><Component /></Router>),
+    history,
+  };
+};
+
 test("renders title", () => {
-  render(
-    <Router>
-      <App />
-    </Router>
-  );
-  const restaurant = screen.getByText(/periodic tables/i);
+  const { getByText } = renderWithHistory(App);
+  const restaurant = getByText(/periodic tables/i);
   expect(restaurant).toBeInTheDocument();
 });
+
