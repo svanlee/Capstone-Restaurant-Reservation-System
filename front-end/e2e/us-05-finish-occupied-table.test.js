408d24d21a53f7df1bb355e1284ede5a80dc954d
Scott van Leeuwen <78891914+svanlee@users.noreply.github.com>
2024-04-29 15:59:05 -0400

    improve

--- front-end/e2e/us-05-finish-occupied-table.test.js
+++ front-end/e2e/us-05-finish-occupied-table.test.js
@@ -2,144 +2,121 @@ const puppeteer = require("puppeteer");
 const { setDefaultOptions } = require('expect-puppeteer');
 const fs = require("fs");
 const fsPromises = fs.promises;
-
-const { containsText } = require("./utils");
+const { expect } = require('@testing-library/puppeteer');
 const { createReservation, createTable } = require("./api");
+const { ReportGenerator } = require('puppeteer-report');
 
 const baseURL = process.env.BASE_URL || "http://localhost:3000";
 
-const onPageConsole = (msg) =>
-  Promise.all(msg.args().map((event) => event.jsonValue())).then((eventJson) =>
-    console.log(`<LOG::page console ${msg.type()}>`, ...eventJson)
-  );
+const report = new ReportGenerator({
+  path: './report',
+  open: true,
+});
 
 describe("US-05 - Finish an occupied table - E2E", () => {
   let page;
   let browser;
+  let reservation;
+  let table;
 
   beforeAll(async () => {
     await fsPromises.mkdir("./.screenshots", { recursive: true });
     setDefaultOptions({ timeout: 1000 });
     browser = await puppeteer.launch();
-  });
-
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  describe("/dashboard page", () => {
-    let reservation;
-    let table;
-
-    beforeEach(async () => {
-      reservation = await createReservation({
-        first_name: "Finish",
-        last_name: Date.now().toString(10),
-        mobile_number: "800-555-1313",
-        reservation_date: "2035-01-01",
-        reservation_time: "13:45",
-        people: 4,
+    report.on('page', (page) => {
+      page.on('console', (msg) => {
+        report.addConsole(msg);
       });
-
-      table = await createTable({
-        table_name: `#${Date.now().toString(10)}`,
-        capacity: 99,
-        reservation_id: reservation.reservation_id,
-      });
-
-      page = await browser.newPage();
-      page.on("console", onPageConsole);
-      await page.setViewport({ width: 1920, height: 1080 });
-      await page.goto(`${baseURL}/dashboard?date=2035-01-01`, {
-        waitUntil: "networkidle0",
-      });
-      await page.reload({ waitUntil: "networkidle0" });
-    });
-
-    test("clicking finish button and then clicking OK makes that table available", async () => {
-      await page.screenshot({
-        path: ".screenshots/us-05-dashboard-finish-button-before.png",
-        fullPage: true,
-      });
-
-      const containsOccupied = await containsText(
-        page,
-        `[data-table-id-status="${table.table_id}"]`,
-        "occupied"
-      );
-
-      expect(containsOccupied).toBe(true);
-
-      const finishButtonSelector = `[data-table-id-finish="${table.table_id}"]`;
-      await page.waitForSelector(finishButtonSelector);
-
-      page.on("dialog", async (dialog) => {
-        expect(dialog.message()).toContain(
+      page.on('dialog', async (dialog) => {
+        report.addDialog(dialog);
+        await expect(dialog.message()).resolves.toContain(
           "Is this table ready to seat new guests?"
         );
         await dialog.accept();
       });
-
-      await page.click(finishButtonSelector);
-
-      await page.waitForResponse((response) => {
-        return response.url().endsWith(`/tables`);
+      page.on('response', (response) => {
+        report.addResponse(response);
       });
-
-      await page.screenshot({
-        path: ".screenshots/us-05-dashboard-finish-button-after.png",
-        fullPage: true,
-      });
-
-      const containsFree = await containsText(
-        page,
-        `[data-table-id-status="${table.table_id}"]`,
-        "free"
-      );
-
-      expect(containsFree).toBe(true);
-    });
-
-    test("clicking finish button and then clicking CANCEL does nothing", async () => {
-      await page.screenshot({
-        path: ".screenshots/us-05-dashboard-finish-button-cancel-before.png",
-        fullPage: true,
-      });
-
-      const containsOccupied = await containsText(
-        page,
-        `[data-table-id-status="${table.table_id}"]`,
-        "occupied"
-      );
-
-      expect(containsOccupied).toBe(true);
-
-      const finishButtonSelector = `[data-table-id-finish="${table.table_id}"]`;
-      await page.waitForSelector(finishButtonSelector);
-
-      page.on("dialog", async (dialog) => {
-        expect(dialog.message()).toContain(
-          "Is this table ready to seat new guests?"
-        );
-        await dialog.dismiss();
-      });
-
-      await page.click(finishButtonSelector);
-
-      await page.waitForTimeout(1000);
-
-      await page.screenshot({
-        path: ".screenshots/us-05-dashboard-finish-button-cancel-after.png",
-        fullPage: true,
-      });
-
-      const containsFree = await containsText(
-        page,
-        `[data-table-id-status="${table.table_id}"]`,
-        "free"
-      );
-
-      expect(containsFree).toBe(false);
     });
   });
+
+  afterAll(async () => {
+    await browser.close();
+    report.generate();
+  });
+
+  beforeEach(async () => {
+    reservation = await createReservation({
+      first_name: "Finish",
+      last_name: Date.now().toString(10),
+      mobile_number: "800-555-1313",
+      reservation_date: "2035-01-01",
+      reservation_time: "13:45",
+      people: 4,
+    });
+
+    table = await createTable({
+      table_name: `#${Date.now().toString(10)}`,
+      capacity: 99,
+      reservation_id: reservation.reservation_id,
+    });
+
+    page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(`${baseURL}/dashboard?date=2035-01-01`, {
+      waitUntil: "networkidle0",
+    });
+    await page.reload({ waitUntil: "networkidle0" });
+  });
+
+  test("clicking finish button and then clicking OK makes that table available", async () => {
+    await page.screenshot({
+      path: ".screenshots/us-05-dashboard-finish-button-before.png",
+      fullPage: true,
+    });
+
+    await expect(page).toMatchElement(`[data-table-id-status="${table.table_id}"]:has-text("occupied")`);
+
+    const finishButtonSelector = `[data-table-id-finish="${table.table_id}"]`;
+    await page.waitForSelector(finishButtonSelector);
+
+    await page.click(finishButtonSelector);
+
+    await page.waitForResponse((response) => {
+      return response.url().endsWith(`/tables`);
+    });
+
+    await page.screenshot({
+      path: ".screenshots/us-05-dashboard-finish-button-after.png",
+      fullPage: true,
+    });
+
+    await expect(page).toMatchElement(`[data-table-id-status="${table.table_id}"]:has-text("free")`);
+  });
+
+  test("clicking finish button and then clicking CANCEL does nothing", async () => {
+    await page.screenshot({
+      path: ".screenshots/us-05-dashboard-finish-button-cancel-before.png",
+      fullPage: true,
+    });
+
+    await expect(page).toMatchElement(`[data-table-id-status="${table.table_id}"]:has-text("occupied")`);
+
+    const finishButtonSelector = `[data-table-id-finish="${table.table_id}"]`;
+    await page.waitForSelector(finishButtonSelector);
+
+    await page.click(finishButtonSelector);
+
+    await page.waitForTimeout(1000);
+
+    await page.screenshot({
+      path: ".screenshots/us-05-dashboard-finish-button-cancel-after.png",
+      fullPage: true,
+    });
+
+    await expect(page).not.toMatchElement(`[data-table-id-status="${table.table_id}"]:has-text("free")`);
+  });
 });
+
+
+npm install @testing-library/puppeteer puppeteer-report
