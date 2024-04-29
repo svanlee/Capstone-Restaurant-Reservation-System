f56937fb2e651679f93c0d8c22c0c13f83c32549
Scott van Leeuwen <78891914+svanlee@users.noreply.github.com>
2024-04-29 15:58:57 -0400

    improve

--- front-end/src/utils/format-reservation-date.js
+++ front-end/src/utils/format-reservation-date.js
@@ -1,19 +1,25 @@
 import { formatAsDate } from "./date-time";
 
 function formatDate(reservation) {
+  if (!reservation || !reservation.reservation_date) {
+    return reservation;
+  }
+
   reservation.reservation_date = formatAsDate(reservation.reservation_date);
   return reservation;
 }
 
 /**
  * Formats the reservation_date property of a reservation.
- * @param reservations
+ * @param {Array<reservation>|reservation} reservations
  *  a single reservation, or an array of reservations.
- * @returns {[reservation]|reservation}
+ * @returns {Array<reservation>|reservation}
  *  the specified reservation(s) with the reservation_date property formatted as YYYY-MM-DD.
  */
 export default function formatReservationDate(reservations) {
-  return Array.isArray(reservations)
-    ? reservations.map(formatDate)
-    : formatDate(reservations);
+  if (Array.isArray(reservations)) {
+    return reservations.map((reservation) => formatDate(reservation));
+  } else {
+    return formatDate(reservations);
+  }
 }
