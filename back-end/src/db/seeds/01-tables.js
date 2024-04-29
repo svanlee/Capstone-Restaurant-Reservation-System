fbbd25385fcd90676a85a3c1c88c05f977e6ecd0
Scott van Leeuwen <78891914+svanlee@users.noreply.github.com>
2024-04-29 15:58:54 -0400

    improve

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -1,9 +1,13 @@
 const tableData = require("./01-tables.json");
 
 exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
-    .then(function () {
+  return knex.schema
+    .dropTableIfExists("tables")
+    .createTable("tables", (table) => {
+      table.increments("id").primary();
+      // add other columns here based on the schema of your JSON data
+    })
+    .then(() => {
       return knex("tables").insert(tableData);
     });
 };
